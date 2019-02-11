af035fa9c01cb10a67194ca7ed753864f1f6e759
sammyreles22 <samuel.reles@gmail.com>
2019-02-10 19:30:14 -0500

    Skeleton for LinkedList
    *

---
+++ AlgorithmsandDataStructs/AlgorithmsLectureOne/LinkedList/src/LinkedList.py
@@ -0,0 +1,39 @@
+class LinkedList:
+    """
+    A class of representing the classical "Linked List" datatype, implementing the algorithms typically seen within a
+    Linked List.
+    """
+    head = None
+
+    is_empty = True
+
+    def append_element(self, element):
+        """
+        A function that allows the user to append an element.
+
+        :param element: The element to be appended
+        """
+
+
+    def remove_element(self, position):
+        """
+        Allows the user to remove an element from the list.
+
+        :param position: The position of the element which is to be removed
+        :return: The element removed from the list
+        """
+
+
+    def reverse_elements(self):
+        """
+        Reverses the list.
+        """
+
+
+    def insert_element(self, position, element):
+        """
+        Inserts an element into the list.
+
+        :param position: The position to which the element should be inserted
+        :param element: The element to be inserted
+        """
